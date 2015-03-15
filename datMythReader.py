8894a8c592664daa787f18f4fa78833c55b9d67f
Young Xemnas <ninzeldagmr@gmail.com>
2015-03-15 15:36:09 -0400

    Add datMythReader.py

---
+++ datMythReader.py
@@ -0,0 +1,19 @@
+datMyth = open('DATMYTH.bmd', 'rb').read()
+myth = datMyth.find('myth')
+locationHex = -1
+numbEntry = str(000).zfill(2) 
+number = 0
+while True:
+    number = number + 1
+    if number < 10:
+        locationHex = datMyth.find('myth_00' + str(number), myth + 1)
+    elif number < 100:
+        locationHex = datMyth.find('myth_0' + str(number), myth + 1)
+    else:
+        locationHex = datMyth.find('myth_' + str(number), myth + 1)
+        
+    if locationHex == -1:
+        print 'Error: Reached end of list'
+        break
+    print datMyth[locationHex:]
+    print datMyth[locationHex+42:]
