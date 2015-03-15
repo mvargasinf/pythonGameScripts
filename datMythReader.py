6f3752ed5361609e31f26f58f311238eecf99277
Young Xemnas <ninzeldagmr@gmail.com>
2015-03-15 19:47:12 -0400

    Almost there! Just need to find a way to get the end of the paragraph.

--- datMythReader.py
+++ datMythReader.py
@@ -1,18 +1,43 @@
+import re, sys
+
+mythWriter = open('MythDAT.txt', 'w') # not working
 datMyth = open('DATMYTH.bmd', 'rb').read()
 myth = datMyth.find('myth')
 locationHex = -1
+locationHexFirst = -1
 number = 0
+numberbefore = 1
+
 while True:
     number = number + 1
+    numberbefore = numberbefore + 1
+    
+    findlessbeforeten = 'myth_00' + str(numberbefore)
+    findlessbeforehund = 'myth_0' + str(numberbefore)
+    findlessbeforelse = 'myth_' + str(numberbefore)
+    
+    findlessten = 'myth_00' + str(number)
+    findlesshund = 'myth_0' + str(number)
+    findelse = 'myth_' + str(number)
+    
+    
     if number < 10:
-        locationHex = datMyth.find('myth_00' + str(number), myth + 1)
+        locationHexFirst = datMyth.find(findlessbeforeten , myth + 1)
+        locationHex = datMyth.find(findlessten, myth + 1)
     elif number < 100:
-        locationHex = datMyth.find('myth_0' + str(number), myth + 1)
+        locationHexFirst = datMyth.find(findlessbeforehund , myth + 1)
+        locationHex = datMyth.find(findlesshund , myth + 1)
     else:
-        locationHex = datMyth.find('myth_' + str(number), myth + 1)
+        locationHexFirst = datMyth.find(findlessbeforelse , myth + 1)
+        locationHex = datMyth.find(findelse, myth + 1)
         
     if locationHex == -1:
         print 'Error: Reached end of list'
-        break
-    print datMyth[locationHex:]
-    print datMyth[locationHex+42:]
+        sys.exit()
+
+    datMyth1 = datMyth[locationHex:locationHex+8] + '\n'
+    datMyth2 = datMyth[locationHex+42:locationHexFirst-2] + '\n' + '\n'
+    print datMyth1
+    print datMyth2
+    mythWriter.write(datMyth1)
+    mythWriter.write(datMyth2)
