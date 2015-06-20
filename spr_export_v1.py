56defcff6fc088b02dd17b799b6ee5d06c0baaee
Young Xemnas <ninzeldagmr@gmail.com>
2015-06-20 13:20:47 -0400

    added completely new scripts for importing and exporting
    I still do Python.

---
+++ spr_export_v1.py
@@ -0,0 +1,56 @@
+number = 0
+firstLocation = 0
+pos = 0
+
+spr_name = raw_input("Name of the SPR you want to extract from? ")
+spr_data = open(spr_name, "rb").read()
+spr_tmx_count = spr_data.count("TMX0")
+
+if spr_tmx_count < 0:
+        print("No tmx instances found, closing...")
+        exit(0)
+
+print(str(spr_tmx_count) + " tmx instances found.")
+all_single = raw_input("Extract all or single? (all or single) ")
+
+if all_single == "all":
+	print("Extracting all tmx files from the spr.... ")
+	while spr_tmx_count > number:
+		number = number + 1
+		firstLocation = spr_data.find("TMX0", firstLocation + 1)
+		read_size = firstLocation - 4
+		read_size = spr_data[read_size : read_size + 2].encode("hex")
+		read_size = int(read_size, 16)
+		tmx_data = firstLocation - 8
+
+		tmx_data = spr_data[tmx_data:firstLocation + read_size]
+		tmxWriter = open(str(number) + ".tmx", "wb")
+		tmxWriter.write(tmx_data)
+		tmxWriter.close()
+	raw_input("Finished! Press enter to exit. ")
+	exit(0)
+
+if all_single == "single":
+	instance = raw_input("What tmx instance do you want to extract? ")
+	if int(instance) > spr_tmx_count:
+		print("Instance does not exist. ")
+		exit(0)
+
+	while number < int(instance):
+		number = number + 1
+		pos = spr_data.find("TMX0", pos + 1)
+
+		# wait for increment clock, then use position data
+		if number == int(instance):
+			tmx_size = pos - 4
+			tmx_size = spr_data[tmx_size:tmx_size+2].encode("hex")
+			tmx_size = int(tmx_size, 16)
+			tmx_data = spr_data[pos-8:pos+tmx_size]
+			tmxWriter = open("extracted.tmx", "wb")
+			tmxWriter.write(tmx_data)
+			tmxWriter.close()
+			raw_input("Finished! Press enter to exit. ")
+			exit(0)
+else:
+	print("Error. Unknown input! ")
+	exit(0)
---
+++ spr_import_v1.py
@@ -0,0 +1,49 @@
+number = 0
+tmxInstancePos = 0
+
+sprName = raw_input("Name of the SPR importing your TMX to? ")
+tmxName = raw_input("Name of the TMX? ")
+sprData = open(sprName, "rb").read()
+tmxData = open(tmxName, "rb").read()
+
+noName = raw_input("Does the SPR file have names that you want to import over? (y,n) ")
+if noName == "y":
+	nameTMX = raw_input("What is the name of the instance you want to import over? ")
+	sprNamePos = sprData.find(nameTMX)
+	tmxNamePos = tmxData.find(nameTMX)
+
+	if sprNamePos and tmxNamePos == -1:
+		print("No name instance found!")
+		exit(0)
+	print("Found name, using new position...")
+	sprNamePos = sprNamePos - 36
+	newSPRName = raw_input("What is the new name for your SPR? ")
+	sprDataWriter = open(newSPRName, "wb")
+	sprDataWriter.write(sprData)
+	sprDataWriter.seek(sprNamePos)
+	sprDataWriter.write(tmxData)
+	sprDataWriter.close()
+	raw_input("Finished! Press enter to exit. ")
+	exit(0)
+
+if noName == "n":
+	tmxInstanceCount = sprData.count("TMX0")
+	print(str(tmxInstanceCount)+ " tmx instances found.")
+	replaceInstance = raw_input("OK, which instance do you want to replace? ")
+	if int(replaceInstance) > tmxInstanceCount:
+		print("Invalid instance! ")
+		exit(0)
+
+	while number < int(replaceInstance):
+		number = number + 1
+		tmxInstancePos = sprData.find("TMX0", tmxInstanceCount + 1)
+		if number == int(replaceInstance):
+			tmxInstancePos = tmxInstancePos - 8
+			newSPRName = raw_input("What is the new name for the SPR? ")
+			sprDataWriter = open(newSPRName, "wb")
+			sprDataWriter.write(sprData)
+			sprDataWriter.seek(tmxInstancePos)
+			sprDataWriter.write(tmxData)
+			sprDataWriter.close()
+	raw_input("Finished! Press enter to exit. ")
+	exit(0)
